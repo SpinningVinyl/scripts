d1f6b66c9995e7bc1367cf0ed91ec665fb78021e
Pavel <31886681+SpinningVinyl@users.noreply.github.com>
2026-04-01 16:59:06 +0100

    Switch to recommended NVIDIA installation method

--- bin/nvidia-install.sh
+++ bin/nvidia-install.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# Install open prebuilt/secureboot-signed Kernel driver
-zypper in nvidia-open-driver-G07-signed-cuda-kmp-default
+# Install NVIDIA repos
+zypper in openSUSE-repos-Tumbleweed-NVIDIA
 
-# Make sure userspace CUDA/Desktop drivers will be in sync with just installed open prebuilt/secureboot-signed Kernel driver
-version=$(rpm -qa --queryformat '%{VERSION}\n' nvidia-open-driver-G07-signed-cuda-kmp-default | cut -d "_" -f1 | sort -u | tail -n 1)
-
-# Install CUDA drivers
-zypper in nvidia-compute-utils-G07 == ${version} nvidia-persistenced == ${version}
-# Install Desktop drivers
-zypper in nvidia-video-G07 == ${version}
+# Install open prebuilt/secureboot-signed Kernel driver and related packages
+zypper in nvidia-open-driver-G07-signed-kmp-meta
