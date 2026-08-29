2803be9fd9fa52736935dc7c712745baad77ff41
Pavel Urusov <plu-github@protonmail.com>
2026-08-29 22:24:36 +0100

    add optional KDE service menus

--- bin/nvidia-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install NVIDIA repos
-zypper in openSUSE-repos-Tumbleweed-NVIDIA
-
-# Install open prebuilt/secureboot-signed Kernel driver and related packages
-zypper in nvidia-open-driver-G07-signed-kmp-meta
